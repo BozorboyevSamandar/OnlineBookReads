d1f5c9f28a8b54f7d9502e73755fabe88004a355
BozorboyevSamandar <bscoders@gmail.com>
2022-11-12 21:45:23 +0500

    create models.py

--- books/models.py
+++ books/models.py
@@ -1,6 +1,34 @@
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+
 
 # Create your models here.
 
 
+class Book(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    isbn = models.CharField(max_length=17)
 
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    email = models.EmailField()
+    bio = models.TextField()
+
+
+class BookAuthor(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+
+class BookReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    comment = models.TextField()
+    stars = models.IntegerField(
+        validators=[MinValueValidator(1),
+                    MaxValueValidator(5)
+                    ])
