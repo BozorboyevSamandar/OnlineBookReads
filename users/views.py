9865ec370562107ba1176f02b7d598d826342d40
BozorboyevSamandar <bscoders@gmail.com>
2022-11-14 13:47:04 +0500

    register view

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 from django.views import View
 
 
@@ -7,6 +8,26 @@ from django.views import View
 class Register(View):
     def get(self, request):
         return render(request, "users/register.html")
+
+    def post(self, request):
+        # create user account
+
+        username = request.POST['username']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = User.objects.create(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+        )
+        user.set_password(password)
+        user.save()
+
+        return redirect("users:login")
 
 
 class LoginView(View):
