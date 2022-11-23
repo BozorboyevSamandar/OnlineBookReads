ed8747a6d9b0bdafb992c21a8173ca0b2cfb79d3
BozorboyevSamandar <bscoders@gmail.com>
2022-11-23 13:53:39 +0500

    crispy-forms

--- OnlineBook/settings.py
+++ OnlineBook/settings.py
@@ -36,9 +36,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # new
+    'crispy_forms',
+    'crispy_bootstrap5',
+    # new
     'books',
     'users',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
