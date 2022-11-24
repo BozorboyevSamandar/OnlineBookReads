7f6800a3dc3df4185f9d04d6da302ef15ddf8015
BozorboyevSamandar <bscoders@gmail.com>
2022-11-24 11:56:37 +0500

    paginator

--- books/views.py
+++ books/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import ListView, DetailView
@@ -10,12 +11,18 @@ class BookListView(ListView):
     template_name = 'books/list.html'
     queryset = Book.objects.all()
     context_object_name = 'books'
+    paginate_by = 2
 
 
 # class BookListView(View):
 #     def get(self, request):
-#         books = Book.objects.all()
-#         context = {'books': books}
+#         books = Book.objects.all().order_by('id')
+#         paginator = Paginator(books, 2)
+#
+#         page_num = request.GET.get('page', 1)
+#         page_obj = paginator.get_page(page_num)
+#
+#         context = {'page_obj': page_obj}
 #         return render(request, 'books/list.html', context)
 
 
