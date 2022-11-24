cb63785dd6e19674200668c99a2bd7c026aed475
BozorboyevSamandar <bscoders@gmail.com>
2022-11-24 12:59:21 +0500

    add search_book forms

--- books/views.py
+++ books/views.py
@@ -7,23 +7,32 @@ from .models import Book
 
 # Create your views here.
 
-class BookListView(ListView):
-    template_name = 'books/list.html'
-    queryset = Book.objects.all()
-    context_object_name = 'books'
-    paginate_by = 2
+# class BookListView(ListView):
+#     template_name = 'books/list.html'
+#     queryset = Book.objects.all()
+#     context_object_name = 'books'
+#     paginate_by = 2
 
 
-# class BookListView(View):
-#     def get(self, request):
-#         books = Book.objects.all().order_by('id')
-#         paginator = Paginator(books, 2)
-#
-#         page_num = request.GET.get('page', 1)
-#         page_obj = paginator.get_page(page_num)
-#
-#         context = {'page_obj': page_obj}
-#         return render(request, 'books/list.html', context)
+class BookListView(View):
+    def get(self, request):
+        books = Book.objects.all().order_by('id')
+
+        search_query = request.GET.get('q', '')
+        if search_query:
+            books = books.filter(title__icontains=search_query)
+
+        page_size = request.GET.get('page_size', 3)
+        paginator = Paginator(books, page_size)
+
+        page_num = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_num)
+
+        context = {
+            'page_obj': page_obj,
+            'search_query': search_query,
+        }
+        return render(request, 'books/list.html', context)
 
 
 class BookDetailView(DetailView):
